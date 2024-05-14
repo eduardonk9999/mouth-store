0b0028ed120ddd026cd24db179214ea8d5f7910e
eduardonk9999 <thiago.novak7@gmail.com>
2024-05-14 08:13:25 -0300

    feat productservice

--- src/main/java/com/mouth/mouth/service/ProductService.java
+++ src/main/java/com/mouth/mouth/service/ProductService.java
@@ -1,5 +1,12 @@
 package com.mouth.mouth.service;
 
+import com.mouth.mouth.repository.ProductRepository;
+
 public class ProductService {
-    private final
+    private final ProductRepository productRepository;
+
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 }
