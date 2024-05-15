061bf1060b49c32469a29c86f02931357ebbe01b
eduardonk9999 <thiago.novak7@gmail.com>
2024-05-15 20:37:32 -0300

    terminando class Product service

--- src/main/java/com/mouth/mouth/service/ProductService.java
+++ src/main/java/com/mouth/mouth/service/ProductService.java
@@ -1,6 +1,9 @@
 package com.mouth.mouth.service;
 
+import com.mouth.mouth.controller.Product;
 import com.mouth.mouth.repository.ProductRepository;
+
+import java.util.List;
 
 public class ProductService {
     private final ProductRepository productRepository;
@@ -8,5 +11,14 @@ public class ProductService {
 
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    public List<Rroduct> listProduct() {
+        return productRepository.findAll();
+    }
+
+    public Ṕroduct registerProduct(Product product) {
+        S save = productRepository.save(product);
+        return save;
     }
 }
